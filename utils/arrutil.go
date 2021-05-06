ffab6439080f62fcbb991cc98cfca364f04945fc
18668179721 <78925880@qq.com>
2021-05-06 17:34:13 +0800

    array tool

--- utils/arrutil.go
+++ utils/arrutil.go
@@ -17,3 +17,20 @@ func AddArrayEx(str_array []string, target string) []string {
 	}
 	return str_array
 }
+
+func RemoveRepeatedElement(arr []string) (newArr []string) {
+	newArr = make([]string, 0)
+	for i := 0; i < len(arr); i++ {
+		repeat := false
+		for j := i + 1; j < len(arr); j++ {
+			if arr[i] == arr[j] {
+				repeat = true
+				break
+			}
+		}
+		if !repeat {
+			newArr = append(newArr, arr[i])
+		}
+	}
+	return
+}
